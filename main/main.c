f749beb9d689e529bbeb44ba71b68b67537da869
Carlos <carlos.santosr@ieee.org>
2018-09-04 17:07:54 -0500

    temp

---
+++ main/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO 22
+#define BLINK_DELAY 1000
+
+void blink_task(void) {
+	gpio_pad_select_gpio(BLINK_GPIO);
+	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+	while (1) {
+		gpio_set_level(BLINK_GPIO, 0);
+		vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
+		gpio_set_level(BLINK_GPIO, 1);
+		vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
+	}
+}
+
+void app_main() {
+	xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+}
