9628bdf076d7fa79f2a87ff1f2f665dbc4f46ed6
Carlos <carlos.santosr@ieee.org>
2018-09-04 18:37:20 -0500

    3 leds blinking
    Yellow led connected to pin 19.
    Red led connected to pin 23.
    Green led connected to pin 18.
    
    These three leds will toggle sequentially.
    
    Status: OK

---
+++ README.md
@@ -0,0 +1,8 @@
+# ESP32_drone
+Yellow led connected to pin 19.
+Red led connected to pin 23.
+Green led connected to pin 18.
+
+These three leds will toggle sequentially.
+
+Status: OK
--- main/main.c
+++ main/main.c
@@ -3,20 +3,28 @@
 #include "freertos/task.h"
 #include "driver/gpio.h"
 
-#define BLINK_GPIO 22
-#define BLINK_DELAY 100
+//#define BLINK_GPIO 22
+//#define BLINK_DELAY 1000
 
-void blink_task(void) {
-	gpio_pad_select_gpio(BLINK_GPIO);
-	gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+uint8_t redPin = 23;
+uint8_t greenPin = 18;
+uint8_t yellowPin = 19;
+
+void blink_task(void * pinNumber) {
+	gpio_pad_select_gpio(*((uint8_t*)pinNumber));
+	gpio_set_direction(*((uint8_t*)pinNumber), GPIO_MODE_OUTPUT);
 	while (1) {
-		gpio_set_level(BLINK_GPIO, 0);
-		vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
-		gpio_set_level(BLINK_GPIO, 1);
-		vTaskDelay(BLINK_DELAY / portTICK_PERIOD_MS);
+		gpio_set_level(*((uint8_t*)pinNumber), 0);
+		vTaskDelay(1000 / portTICK_PERIOD_MS / 2);
+		gpio_set_level(*((uint8_t*)pinNumber), 1);
+		vTaskDelay(1000 / portTICK_PERIOD_MS / 2);
 	}
 }
 
 void app_main() {
-	xTaskCreate(&blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL);
+	xTaskCreate(&blink_task, "blink_task_yellow", configMINIMAL_STACK_SIZE, (void*)&yellowPin, 5, NULL);
+	vTaskDelay(166 / portTICK_PERIOD_MS);
+	xTaskCreate(&blink_task, "blink_task_red", configMINIMAL_STACK_SIZE, (void*)&redPin, 5, NULL);
+	vTaskDelay(166 / portTICK_PERIOD_MS);
+	xTaskCreate(&blink_task, "blink_task_green", configMINIMAL_STACK_SIZE, (void*)&greenPin, 5, NULL);
 }
